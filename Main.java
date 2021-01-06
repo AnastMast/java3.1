4088ece2ca90cee1c0cd3530a8c616a5504fdccc
Anastasiia Matveeva <eglekato@gmail.com>
2021-01-06 19:08:05 +0300

    add Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        long amount = 92000;
+        long amountmili = 20;
+        long bonus = amount / amountmili;
+        System.out.println(bonus);
+
+
+    }
+}
